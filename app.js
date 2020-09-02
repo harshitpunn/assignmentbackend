7be9eb1375d80a7fd6fb5a698c9051f72a84ca69
Harshit <harshitpunn@gmail.com>
2020-09-02 10:56:12 +0530

    FIXED server

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'dist')))
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname,'dist/index.html'))
 })
