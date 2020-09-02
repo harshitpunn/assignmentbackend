028aada6321ff82e9b0b9d65906a6c1f2f000b8a
Harshit <harshitpunn@gmail.com>
2020-09-02 10:42:46 +0530

    Deployment of code on heroku

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongose = require('mongoose');
+const path  =   require('path');
+
+app.use(cors());
+app.options('*', cors());
+
+// morgan is logging framework for nodejs
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Headers', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+app.use(express.static(path.join(__dirname,'public')))
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname,'public/index.html'))
+})
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+
+module.exports = app;
